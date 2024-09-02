070644891c1a39ba4abaf0b369efa2b6afecaf96
guillaume prof <guillaume.saintetienne@iter.org>
2024-09-02 20:01:25 +0200

    responsabilité ParcMetre

--- src/main/kotlin/parcmetre/domain/entities/ParcMetre.kt
+++ src/main/kotlin/parcmetre/domain/entities/ParcMetre.kt
@@ -1,9 +1,12 @@
 package parcmetre.domain.entities
 
-class ParcMetre() {
+class ParcMetre(val Id: String, val codeDeZone: String) {
+
 
     //que fait il / quelles sont ses responsabilités
-
+    fun EditerTicker(duree: Int): Ticket {
+        TODO()
+    }
 
 
 
--- src/test/kotlin/exercice_1/regles_metier/1_TestParcmetre.kt
+++ src/test/kotlin/exercice_1/regles_metier/1_TestParcmetre.kt
@@ -1,8 +1,26 @@
 package exercice_1.regles_metier
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.datetime.*
+import parcmetre.domain.entities.ParcMetre
 
 class `1_TestParcmetre` : StringSpec({
+
+    "je veux prendre un ticket au parcemetre pour 30 minutes"  {
+        val parcmetre = ParcMetre("00001", "CentreVille")
+
+        val ticket  = parcmetre.EditerTicker(duree = 30)
+
+        ticket.dureeDeStationnmentEnMinutes shouldBe  30
+
+        //ca marchera pas
+        ticket.heureEntree shouldBe Clock.System.now().toLocalDateTime( TimeZone.UTC)
+
+
+        //    .plus( DateTimePeriod(minutes = 30) , TimeZone.UTC)
+    }
 
     "Quand  je prends un ticket, alors il est daté au moment où il est demandé ".config(enabled = true) {
         // Arrange / Given
